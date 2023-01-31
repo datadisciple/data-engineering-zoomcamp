1bba84491c1eec5c5c590629471816d4ee3799d7
datadisciple <sean.meehan22@gmail.com>
2023-01-31 01:18:16 +0000

    adds ingest_data.py

--- week_2_workflow_orchestration/prefect-demo/ingest_data.py
+++ week_2_workflow_orchestration/prefect-demo/ingest_data.py
@@ -1,24 +1,10 @@
-#!/usr/bin/env python
-# coding: utf-8
-
 import os
-import argparse
-
 from time import time
-
 import pandas as pd
 from sqlalchemy import create_engine
 
+def ingest_data(user, password, host, port, db, table_name, url):
 
-def main(params):
-    user = params.user
-    password = params.password
-    host = params.host 
-    port = params.port 
-    db = params.db
-    table_name = params.table_name
-    url = params.url
-    
     # the backup files are gzipped, and it's important to keep the correct extension
     # for pandas to be able to open the file
     if url.endswith('.csv.gz'):
@@ -62,17 +48,16 @@ def main(params):
             print("Finished ingesting data into the postgres database")
             break
 
+def main():
+    user = "root"
+    password = "root"
+    host = "localhost"
+    port = "5432"
+    db = "ny_taxi"
+    table_name = "yellow_taxi_trips"
+    csv_url = "https://github.com/DataTalksClub/nyc-tlc-data/releases/download/yellow/yellow_tripdata_2021-01.csv.gz"
+
+    ingest_data(user, password, host, port, db, table_name, csv_url)
+
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Ingest CSV data to Postgres')
-
-    parser.add_argument('--user', required=True, help='user name for postgres')
-    parser.add_argument('--password', required=True, help='password for postgres')
-    parser.add_argument('--host', required=True, help='host for postgres')
-    parser.add_argument('--port', required=True, help='port for postgres')
-    parser.add_argument('--db', required=True, help='database name for postgres')
-    parser.add_argument('--table_name', required=True, help='name of the table where we will write the results to')
-    parser.add_argument('--url', required=True, help='url of the csv file')
-
-    args = parser.parse_args()
-
-    main(args)
+    main()
