02dcbf37da9853d11124cd3750eefdef00e5738b
datadisciple <sean.meehan22@gmail.com>
2023-02-18 03:23:43 +0000

    updates path to save local data to be from root repo folder

--- week_2_workflow_orchestration/prefect-demo/flows/03_deployments/parameterized_flow.py
+++ week_2_workflow_orchestration/prefect-demo/flows/03_deployments/parameterized_flow.py
@@ -38,7 +38,7 @@ def clean(df: pd.DataFrame, color: str) -> pd.DataFrame:
 @task()
 def write_local(df: pd.DataFrame, color: str, dataset_file: str) -> Path:
     """Write DataFrame out locally as parquet file"""
-    path = Path(f"data/{color}/{dataset_file}.parquet")
+    path = Path(f"week_2_workflow_orchestration/prefect-demo/{color}/{dataset_file}.parquet")
     df.to_parquet(path, compression="gzip")
     return path
 
